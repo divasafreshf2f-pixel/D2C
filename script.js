21ee5ee4dd440249b4ff1ba9e3f6df07651df326
divasafreshf2f-pixel <divasafreshf2f@gmail.com>
2025-12-31 12:02:44 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,1 @@
-document.addEventListener("DOMContentLoaded", function () {
-  const btn = document.querySelector(".mobile-drop-btn");
-  const dropdown = document.querySelector(".mobile-dropdown");
 
-  btn.addEventListener("click", function () {
-    dropdown.classList.toggle("active");
-  });
-});
